895a9fb7eb31bfc9af61141190d49436fc08a99e
kriss-000 <0buck.tooth0@gmail.com>
2024-09-04 15:58:06 +0000

    final commit.

--- challenge1.py
+++ challenge1.py
@@ -1,1 +1,24 @@
-message = " A normal day in TCAT consists of several activities, usually pretaining to the class one is taking. I took automotive m"
+message = "A normal day in TCAT consists of several activities, usually pretaining to the class one is taking."
+#Whenever I type print(message), the message will print in the terminal
+print(message)
+message = "I took automotive when I went to TCAT, in which I assisted with changing oil, filling in dents, changing tires. etc."
+#Whenever I type print(message), the message will print in the terminal
+print(message)
+message = "The adult students did more difficult jobs."
+#Whenever I type print(message), the message will print in the terminal
+print(message)
+message = "Such as welding, changing gaskets, changing brakes, etc."
+#Whenever I type print(message), the message will print in the terminal
+print(message)
+message = "They woWhenever I type print(message), the message will print in the terminaluld complete paper and book assignments as well, but the high school students would not."
+#Whenever I type print(message), the message will print in the terminal
+print(message)
+message = "Every now and then all students would be tasked with cleaning the shop."
+#Whenever I type print(message), the message will print in the terminal
+print(message)
+message = "Tools had to be put up, messes had to be dealt with, and every station had to be readied for the next vehicle coming through."
+#Whenever I type print(message), the message will print in the terminal
+print(message)
+message = "The TCAT instructors were extremely particular on how they wanted their shop to look."
+#Whenever I type print(message), the message will print in the terminal
+print(message)
