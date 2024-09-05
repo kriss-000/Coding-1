2373cd898064ef6476dc79ee987dd87b2de0a52c
kriss-000 <0buck.tooth0@gmail.com>
2024-09-05 16:30:03 +0000

    Updated code

--- challenge1.py
+++ challenge1.py
@@ -22,3 +22,53 @@ print(message)
 message = "The TCAT instructors were extremely particular on how they wanted their shop to look."
 #Whenever I type print(message), the message will print in the terminal
 print(message)
+first_name = 'kriss'
+print (first_name)
+print (first_name.title())
+first_name = 'kriss'
+#Typing this into the terminal will print my name
+print (first_name)
+print (first_name.tittle())
+print (first_name.upper())
+first_name = 'kriss'
+#Typing this into the terminal will print my name in lowercase
+print (first_name.lower())
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = first_name +''+ last_name
+message = full_name.title()+''+"was the first computer programer"
+print (message)
+print ("Hello everyone!")
+print ("\tHello everyone!")
+print ("Hello \teveryone!")
+print ("Hello everyone!")
+print ("\nHello everyone!")
+print ("Hello \neveryone1")
+print ("\n\n\nHello everyone!")
+name = 'kriss'
+print ('-'name.lstrip())
+print ('-'name.rstrip())
+print ('-'name.rstrip())
+#Typing this will output the following mathematical equation into the terminal
+print (3+3)
+print (3-2)
+print (3*2)
+print (3/2)
+print (3**2)
+standard_order = 2+3*4
+print (standard_order)
+my_order = (2+3)*4
+print (my_order)
+print (0.1+0.1)
+print (0.1+0.2)
+print (3*0.1)
+# python 3.3
+print (4/2)
+# python 3.3
+print (3/2)
+# this line is a comment
+print("this line is not a comment, it is code")
+import this
+# i learned how to strip whitespace from strings
+name = '\t\teric'
+print("i can strip tabs from my name: " + name.strip())
