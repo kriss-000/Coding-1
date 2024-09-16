5c7d3fab6556b02807f8f14ace460e6eb5f8889d
kriss-000 <0buck.tooth0@gmail.com>
2024-09-16 17:09:45 +0000

    final commit.

---
+++ quiz1.py
@@ -0,0 +1,7 @@
+message = "I am taking a Coding 1 quiz. My teacher is Coach Martin. It is in my fourth period."
+print(message)
+print (message.title())
+print (message.upper())
+print (message.lower())
+numbers = ['1', '2', '3', '4']
+total = sum(numbers)
