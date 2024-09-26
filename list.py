0f25fabcba531505949070822122f60c86ea595b
kriss-000 <0buck.tooth0@gmail.com>
2024-09-26 16:36:13 +0000

    final commit.

---
+++ list.py
@@ -0,0 +1,16 @@
+students = ['bernice', 'aaron', 'cody']
+print("Hello, " + student.title() + "!")
+dogs = ['border collie', 'australian cattle dog', 'labrador retriever']
+dog = dogs[0]
+print(dog.title())
+dogs = ['border collie', 'australian cattle dog', 'labrador retriever']
+dog = dogs[1]
+print(dog.title())
+dogs = ['border collie', 'australian cattle dog', 'labrador retriever']
+dog = dogs[-1]
+print(dog.title())
+dogs = ['border collie', 'australian cattle dog', 'labrador retriever']
+dog = dogs[-2]
+print(dog.title())
+dogs = ['border collie', 'australian cattle dog', 'labrador retriever']
+dog = dogs[-4]
