8ac2b2857502e7a02131a931d5022e5e29c462d9
kriss-000 <0buck.tooth0@gmail.com>
2024-11-01 17:14:25 +0000

    final commit.

---
+++ if.py
@@ -0,0 +1,69 @@
+desserts = ['ice cream', 'chocolate', 'apple crisp', 'cookies']
+favorite_dessert = 'apple crisp'
+for dessert in desserts :
+    if dessert == favorite_dessert :
+        print("%s is my favorte dessert!" % dessert.title())
+    else:
+        print("I like %s." % dessert)
+5 == 5
+3 == 5
+5 == 5.0
+'eric' = 'eric'
+'Eric' = 'eric'
+'Eric'.lower() == 'eric'.lower()
+'5' == 5
+'5' == str(5)
+3 != 5
+5 != 5
+'Eric' != 'eric'
+5 > 3
+5 >= 3
+3 >= 3
+3 < 5
+3 <= 5
+3 <= 3
+vowels = ['a', 'e', 'i', 'o', 'u']
+'a' in vowels
+vowels = ['a', 'e', 'i', 'o', 'u']
+'b' in vowels
+dogs = ['willie', 'hootz', 'peso', 'juno']
+if len(dogs) > 3:
+    print("Wow, we have a lot of dogs here!")
+dogs = ['willie', 'hootz']
+if len(dogs) > 3:
+    print("Wow, we have a lot of dogs here!")
+dogs = ['willie', 'hootz', 'peso', 'juno']
+if len(dogs) > 3:
+    print("Wow, we have a lot of dogs here!")
+else:
+    print("Okay, this is a reasonable number of dogs.")
+dogs = ['willie', 'hootz']
+if len(dogs) > 3:
+    print("Wow, we have a lot of dogs here!")
+else:
+    print("Okay, this is a reasonable number of dogs.")
+dogs = ['willie', 'hootz', 'peso', 'monty', 'juno', 'turkey']
+if len(dogs) >= 5:
+    print("Holy mackerel, we might as well start a dog hostel!")
+elif len(dogs) >= 3:
+    print("Wow, we have a lot of dogs here!")
+else:
+    print("Okay, this is a reasonable number of dogs.")
+dogs = ['willie', 'hootz', 'peso', 'monty']
+if len(dogs) >= 5:
+    print("Holy mackerel, we might as well start a dog hostel!")
+elif len(dogs) >= 3:
+    print("Wow, we have a lot of dogs here!")
+else:
+    print("Okay, this is a reasonable number of dogs.")
+dogs = ['willie', 'hootz']
+if len(dogs) >= 5:
+    print("Holy mackerel, we might as well start a dog hostel!")
+elif len(dogs) >= 3:
+    print("Wow, we have a lot of dogs here!")
+else:
+    print("Okay, this is a reasonable number of dogs.")
+dogs = []
+if len(dogs) >= 5:
+    print("Holy mackerel, we might as well start a dog hostel!")
+elif len(dogs) >= 3:
