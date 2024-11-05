36cbe008357c0e0ac47e88cbc966243fb7ada4dd
kriss-000 <0buck.tooth0@gmail.com>
2024-11-05 17:15:18 +0000

    CHallenge3.py final commit.

--- challenge3.py
+++ challenge3.py
@@ -1,4 +1,4 @@
-ylist = ["R", "G", "B"]
+list = ["R", "G", "B", "B", "G", "R", "R", "G", "B", "B"]
 sum = 0
 for x in list:
     if x == "R": 
@@ -8,4 +8,4 @@ for x in list:
     elif x == "B": 
         sum = sum + 20
 print(sum)
-# The sum of the aliens is 35
+# The sum of the aliens is 55
