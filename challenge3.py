b2602d91bd5bb30e8430c875922cbb1be7fef685
kriss-000 <0buck.tooth0@gmail.com>
2024-11-05 16:57:38 +0000

    final commit.

---
+++ challenge3.py
@@ -0,0 +1,11 @@
+list = ["R", "G", "B"]
+sum = 0
+for x in list:
+    if x == "R": 
+        sum = + 5
+    elif x == "G": 
+        sum = sum + 10
+    elif x == "B": 
+        sum = sum + 20
+print(sum)
+# The sum of the aliens is 35
